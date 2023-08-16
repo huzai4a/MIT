679e4be7f8bedf78043a55ec7b3fdcc532aa1009
Huzaifa Syed <huzaifa_syed78@hotmail.com>
2023-08-16 04:09:43 -0400

    made working trigger config file

--- ps5/test.py
+++ ps5/test.py
@@ -435,12 +435,13 @@ def read_trigger_config(filename):
     trigDict = {}
     trigList = []
 
+
     # for each input line
     for inputs in lines:
         # split into temporary list at each comma
         tempList = inputs.split(",")
         # if first input in line was ADD then add to list
-        if tempList[0].upper() == "ADD":
+        if tempList[0] == "ADD":
             # adds each listed trigger to the trigger list
             for i in range(1, len(tempList)):
                 trigList.append(trigDict[tempList[i]])
@@ -462,78 +463,5 @@ def read_trigger_config(filename):
 
     # returns the list of triggers
     return trigList
-
-
-
-SLEEPTIME = 5 #seconds -- how often we poll
-
-def main_thread(master):
-    # A sample trigger list - you might need to change the phrases to correspond
-    # to what is currently in the news
-    try:
-        t1 = TitleTrigger("election")
-        t2 = DescriptionTrigger("trump")
-        t3 = DescriptionTrigger("clinton")
-        t4 = OrTrigger(t2, t3)
-        triggerlist = [t1, t4]
-
-        # Problem 11
-        # TODO: After implementing read_trigger_config, uncomment this line 
-        triggerlist = read_trigger_config(Path(__file__).with_name('triggers.txt'))
-        
-        # HELPER CODE - you don't need to understand this!
-        # Draws the popup window that displays the filtered stories
-        # Retrieves and filters the stories from the RSS feeds
-        frame = Frame(master)
-        frame.pack(side=BOTTOM)
-        scrollbar = Scrollbar(master)
-        scrollbar.pack(side=RIGHT,fill=Y)
-
-        t = "Google & Yahoo Top News"
-        title = StringVar()
-        title.set(t)
-        ttl = Label(master, textvariable=title, font=("Helvetica", 18))
-        ttl.pack(side=TOP)
-        cont = Text(master, font=("Helvetica",14), yscrollcommand=scrollbar.set)
-        cont.pack(side=BOTTOM)
-        cont.tag_config("title", justify='center')
-        button = Button(frame, text="Exit", command=root.destroy)
-        button.pack(side=BOTTOM)
-        guidShown = []
-        def get_cont(newstory):
-            if newstory.get_guid() not in guidShown:
-                cont.insert(END, newstory.get_title()+"\n", "title")
-                cont.insert(END, "\n---------------------------------------------------------------\n", "title")
-                cont.insert(END, newstory.get_description())
-                cont.insert(END, "\n*********************************************************************\n", "title")
-                guidShown.append(newstory.get_guid())
-
-        while True:
-
-            print("Polling . . .", end=' ')
-            # Get stories from Google's Top Stories RSS news feed
-            stories = process("http://news.google.com/news?output=rss")
-
-            # Get stories from Yahoo's Top Stories RSS news feed
-            # stories.extend(process("http://news.yahoo.com/rss/topstories"))
-
-            stories = filter_stories(stories, triggerlist)
-
-            list(map(get_cont, stories))
-            scrollbar.config(command=cont.yview)
-
-
-            print("Sleeping...")
-            time.sleep(SLEEPTIME)
-
-    except Exception as e:
-        print(e)
-
-
-if __name__ == '__main__':
-    root = Tk()
-    root.title("Some RSS parser")
-    t = threading.Thread(target=main_thread, args=(root,))
-    t.start()
-    root.mainloop()
-
+    
+print(read_trigger_config(Path(__file__).with_name('triggers.txt')))
